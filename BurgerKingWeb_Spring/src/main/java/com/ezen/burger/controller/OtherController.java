2c51c83678d581487e501e82ad3f70c880ae533e
Java02 <Java02@DESKTOP-RDLU2MK>
2022-01-03 19:57:55 +0900

    Delivery LoginPage Update(not complete)

--- BurgerKingWeb_Spring/src/main/java/com/ezen/burger/controller/OtherController.java
+++ BurgerKingWeb_Spring/src/main/java/com/ezen/burger/controller/OtherController.java
@@ -2,6 +2,7 @@ package com.ezen.burger.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ezen.burger.service.OtherService;
 
@@ -9,4 +10,19 @@ import com.ezen.burger.service.OtherService;
 public class OtherController {
 	@Autowired
 	OtherService os;
+	
+	@RequestMapping(value="/")
+	public String index() {
+		return "redirect:/main";
+	}
+	
+	@RequestMapping(value="/main")
+	public String main() {
+		return "main";
+	}
+	
+	@RequestMapping(value="/admin")
+	public String admin() {
+		return "admin/admin";
+	}
 }
