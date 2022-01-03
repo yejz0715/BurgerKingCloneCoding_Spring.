40e0d92c21a9e60d5bc0c943028eaa4b1fada322
JAVA02 <JAVA02@DESKTOP-UEE8H2B>
2022-01-03 20:09:11 +0900

    eventList add

--- BurgerKingWeb_Spring/src/main/java/com/ezen/burger/controller/EventController.java
+++ BurgerKingWeb_Spring/src/main/java/com/ezen/burger/controller/EventController.java
@@ -2,6 +2,8 @@ package com.ezen.burger.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ezen.burger.service.EventService;
 
@@ -9,4 +11,11 @@ import com.ezen.burger.service.EventService;
 public class EventController {
 	@Autowired
 	EventService es;
+	
+	@RequestMapping("/eventListForm")
+	   public String eventListForm(Model model) {
+	   
+	      
+	      return "redirect:/event/eventListForm";
+	   }
 }
