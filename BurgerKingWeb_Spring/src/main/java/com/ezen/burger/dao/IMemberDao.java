43524e65000729b5e5eb47016240cc4def7881e4
Java02 <Java02@DESKTOP-RDLU2MK>
2022-01-03 21:10:21 +0900

    Login, Logout Method mapping Update

--- BurgerKingWeb_Spring/src/main/java/com/ezen/burger/dao/IMemberDao.java
+++ BurgerKingWeb_Spring/src/main/java/com/ezen/burger/dao/IMemberDao.java
@@ -2,7 +2,9 @@ package com.ezen.burger.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.ezen.burger.dto.MemberVO;
+
 @Mapper
 public interface IMemberDao {
-	
+	public MemberVO getMember(String id);
 }
--- BurgerKingWeb_Spring/src/main/java/com/ezen/burger/service/MemberService.java
+++ BurgerKingWeb_Spring/src/main/java/com/ezen/burger/service/MemberService.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ezen.burger.dao.IMemberDao;
+import com.ezen.burger.dto.MemberVO;
 
 @Service
 public class MemberService {
 	@Autowired
 	IMemberDao mdao;
+
+	public MemberVO getMember(String id) {
+		return mdao.getMember(id);
+	}
 }
