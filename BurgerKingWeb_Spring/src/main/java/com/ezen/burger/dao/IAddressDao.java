0be7ed1f7d193a316055b0c2dbf29a1de0efdead
YataNox <rladnwls0523@naver.com>
2022-01-05 20:13:23 +0900

    Member, Guest Address init Part Write

--- BurgerKingWeb_Spring/src/main/java/com/ezen/burger/dao/IAddressDao.java
+++ BurgerKingWeb_Spring/src/main/java/com/ezen/burger/dao/IAddressDao.java
@@ -1,12 +1,20 @@
 package com.ezen.burger.dao;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import com.ezen.burger.dto.AddressVO;
+import com.ezen.burger.dto.MemberVO;
 import com.ezen.burger.dto.MyAddressVO;
 
 @Mapper
 public interface IAddressDao {
 
 	public MyAddressVO getMyAddress(int mseq);
+
+	public ArrayList<AddressVO> selectAddressByDong(String dong);
+
+	public void setUserAddress(MyAddressVO mavo);
 	
 }
